d4005d0baebfb97d70e0be8c555162397ef97650
Ashishjayandar <ashishjayandar@gmail.com>
2017-07-22 22:43:51 -0500

    initial commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,17 @@
+#Employee Verification 
+
+#Data Collection
+puts "Please enter your name"
+usr_name = gets.chomp
+puts "How old are you?"
+usr_age= gets.chomp
+puts "What year were you born?"
+usr_year= gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Yes/No)"
+usr_choice = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?(Yes/No)"
+usr_insurance = gets.chomp
+
+#Validation_checks
+
+#age_check
