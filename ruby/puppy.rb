31a9a945bc8ed86c263986cfd00f5b44da67ff6b
Ashishjayandar <ashishjayandar@gmail.com>
2017-07-31 14:41:36 -0500

    puppy file updated

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES 
+#Doggilaecious Puppyobilis
+
+# CHARACTERISTICS 
+# height: varies
+# Friendly: yes
+# Eye count: 2
+# Favorite thing to do: drag garbage all over the house
+#
+# Name: varies
+# Color: varies
+
+# BEHAVIOR 
+# Jump
+# Hug
+# Snugger
