65eb7d575a974fc44f37ed477531c6043250e65a
Ashishjayandar <ashishjayandar@gmail.com>
2017-08-09 11:19:37 -0700

    the car object created and was added with behaviours

--- js/data_structure.js
+++ js/data_structure.js
@@ -13,3 +13,28 @@ for(var i=0;i<color.length;i++)
 	horse[name[i]]=color[i];
 }
 console.log(horse);
+
+function Car(model,color,automatic_shift,max_speed)
+{
+	this.model=model;
+	this.color=color;
+	this.automatic_shift=automatic_shift;
+	this.max_speed=max_speed;
+
+	this.makecar=function(){console.log("the car is made..!")};
+	this.shift=function(){
+		if (this.automatic_shift){
+			console.log("Anyone can drive this car");
+		}
+		else{
+			console.log("Please learn about the manual shift before driving the car");
+		}
+	}
+}
+
+
+var mycar= new Car("BMW","black",true,300);
+console.log(mycar.model);
+//console.log(mycar);
+mycar.makecar();
+mycar.shift();
