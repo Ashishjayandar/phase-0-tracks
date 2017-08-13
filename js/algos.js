4d954e24bd286ea156d7a2cf0741c632331de2a6
Ashishjayandar <ashishjayandar@gmail.com>
2017-08-12 20:35:13 -0700

    the random words generator created and updated

---
+++ js/algos.js
@@ -0,0 +1,52 @@
+function longeststring(array){
+	var long="";
+	var len=array.length
+	for (var i = 0; i < len; i++)
+	{
+		if (array[i].length > long.length)
+		{
+			long=array[i];
+
+		}
+	} 
+	console.log("the longest string in the given array is "+long);
+};
+function compare_objects(obj1,obj2){
+	for(x in obj1)
+	{
+		if (obj1[x] == obj2[x])
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+};
+function randomword(num){
+	var alpha="abcdefghijklmnopqrstuvwxyz";
+	var test="";
+	var array=[];
+	for(var i=0;i<num;i++)
+	{	
+		var k=Math.floor(Math.random()*10)+1;
+		test = "";
+	 	for(var j=0;j<k;j++)
+		{
+			test+=alpha.charAt(Math.floor(Math.random()*alpha.length));
+		}
+		array.push(test);
+	}
+	return array;
+};
+
+console.log(randomword(10));
+
+
+// var dog1={name: "Spot", age: 54}
+// var dog2={name: "Spot", gooddog:true}
+
+// console.log(compare_objects(dog1,dog2));
+// longeststring(["long phrase","longest phrase","longer phrase"]);
+
